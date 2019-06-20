bcc3f91b6ade9592e6e4f162e0d9398038e23f96
mjfalahee <mjfalahee@gmail.com>
2019-06-20 14:44:32 -0700

    Mock database setup and functioning, moving onto tests

--- data/migrations/20190620141723_superheroes.js
+++ data/migrations/20190620141723_superheroes.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('superheroes', tbl => {
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+        tbl.string('power', 255).notNullable();
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('superheroes');
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-//import routes
+const heroDB = require('./data/models/superheroesModel');
 
 const server = express();
 server.use(express.json());
@@ -11,7 +11,46 @@ server.get('/', (req, res) => {
     });
 });
 
-//api routes
+server.get('/api/superheroes', (req, res) => {
+    heroDB.getAll()
+        .then(heroes => {
+            res.status(200).json(heroes);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
+
+server.get('/api/superheroes/:id', (req, res) => {
+    heroDB.findById(req.params.id)
+        .then(hero => {
+            res.status(200).json(hero);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
+
+server.post('/api/superheroes', (req, res) => {
+    heroDB.insert(req.body)
+        .then(hero => {
+            res.status(201).json(hero);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
+
+server.delete('/api/superheroes/:id', (req, res) => {
+    heroDB.remove(req.params.id)
+        .then(hero => {
+            res.status(200).json({
+                message: `${hero} hero removed from the database`});
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
 
 
 module.exports = server;
